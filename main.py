80439c743f11fb0ba302c2d9e25fadbb174978b8
David Peter <david.a.peter@gmail.com>
2011-06-24 22:35:44 -0400

    Refactoring and added new templates/css.

--- main.py
+++ main.py
@@ -11,33 +11,40 @@ PORT = 8888
 db = None
 
 class MainHandler(tornado.web.RequestHandler):
-	def get(self):
-		sess = db.get_session()
-		all_books = [b for b in sess.query(Book)]
-		self.render("index.html", all_books = all_books)
+  def get(self):
+    sess = db.get_session()
+    all_books = [b for b in sess.query(Book)]
+    self.render("index.html", all_books = all_books)
 
-	def post(self):
-		new_book = self.get_argument("book_name")
-		db.add_book(new_book)
-		self.redirect("/")
+  def post(self):
+    new_book = self.get_argument("book_name")
+    db.add_book(new_book)
+    self.redirect("/")
+
+class TopTenHandler(tornado.web.RequestHandler):
+  def get(self):
+    sess = db.get_session()
+    top_ten_books = sess.query(Book).order_by(Book.rating)[:10]
+    self.render("top-10.html", top_ten_books = top_ten_books)
 
 class Application(tornado.web.Application):
-	def __init__(self):
-		handlers  = [ (r"/", MainHandler)
-				        ,
-								]
+  def __init__(self):
+    handlers  = [ (r"/", MainHandler),
+                  (r"/top-10", TopTenHandler),
+                ]
 
-		settings = dict(
-			template_path = os.path.join(os.path.dirname(__file__), "templates"),
-			static_path = os.path.join(os.path.dirname(__file__), "static"),
-		)
+    settings = dict(
+      template_path = os.path.join(os.path.dirname(__file__), "templates"),
+      static_path = os.path.join(os.path.dirname(__file__), "public"),
+    )
 
-		db = DBWrapper() #initialize DB
+    global db
+    db = DBWrapper() #initialize DB
 
-		tornado.web.Application.__init__(self, handlers, **settings)
+    tornado.web.Application.__init__(self, handlers, **settings)
 
 if __name__ == "__main__":
-	print "http://localhost:%d" % (PORT)
-	server = tornado.httpserver.HTTPServer(Application())
-	server.listen(PORT)
-	tornado.ioloop.IOLoop.instance().start()
+  print "http://localhost:%d" % (PORT)
+  server = tornado.httpserver.HTTPServer(Application())
+  server.listen(PORT)
+  tornado.ioloop.IOLoop.instance().start()
